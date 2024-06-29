c6e696184c58a55fc07fb3e40ea1bfa286826ea6
oysandvik94 <oysandvik94@gmail.com>
2024-06-29 10:54:01 +0200

    test: add more tests for simple cases

--- tests/parser/parser_spec.lua
+++ tests/parser/parser_spec.lua
@@ -1,3 +1,5 @@
+local parser = require("curl.parser")
+
 local function assert_commands(expected, received)
 	local expected_silenced = ""
 	if #expected > 0 then
@@ -9,8 +11,6 @@ local function assert_commands(expected, received)
 end
 
 describe("Able to parse simple buffer", function()
-	local parser = require("curl.parser")
-
 	it("containing zero curl command", function()
 		local input_buffer = {}
 		local cursor_pos = 1
@@ -43,6 +43,49 @@ describe("Able to parse simple buffer", function()
 			local parsed_command = parser.parse_curl_command(index, input_buffer)
 
 			assert_commands(curl_line, parsed_command)
+		end
+	end)
+
+	it("containing many spaced curl commands", function()
+		local input_buffer = {
+			"",
+			"curl https://first.com/todos/1",
+			"",
+			"curl https://second.com/todos/1",
+			"",
+			"curl https://third.com/todos/1",
+			"",
+		}
+
+		assert_commands(input_buffer[2], parser.parse_curl_command(2, input_buffer))
+		assert_commands(input_buffer[4], parser.parse_curl_command(4, input_buffer))
+		assert_commands(input_buffer[6], parser.parse_curl_command(6, input_buffer))
+	end)
+
+	it("containing multiline curl-command", function()
+		local input_buffer = {
+			"curl https://first.com/todos/1",
+			"curl -X POST https://jsonplaceholder.typicode.com/posts",
+			"-H 'Content-Type: application/json'",
+			"-d",
+			"'{",
+			'"title": "foo",',
+			'"body": "bar",',
+			'"userId": 123',
+			"}'",
+			"curl https://third.com/todos/1",
+		}
+
+		assert_commands(input_buffer[1], parser.parse_curl_command(1, input_buffer))
+		assert_commands(input_buffer[#input_buffer], parser.parse_curl_command(#input_buffer, input_buffer))
+
+		-- All cursor positions in the multiline curl is valid
+		local expected_command =
+			'curl -X POST https://jsonplaceholder.typicode.com/posts -H \'Content-Type: application/json\' -d \'{ "title": "foo", "body": "bar", "userId": 123 }\''
+
+		for index = 2, 9 do
+			local parsed_command = parser.parse_curl_command(index, input_buffer)
+			assert_commands(expected_command, parsed_command)
 		end
 	end)
 end)
