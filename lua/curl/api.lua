8a7eaf3bc8bfbbe1f68c44c0f6c41b73aaecba8b
oysandvik94 <oysandvik94@gmail.com>
2024-06-29 12:15:28 +0200

    refactor: move logic for output buffer to its own buffer

--- lua/curl/api.lua
+++ lua/curl/api.lua
@@ -1,34 +1,12 @@
 local M = {}
 
-local cache = require("curl.cache")
 local parser = require("curl.parser")
 local buffers = require("curl.buffers")
-local notify = require("curl.notifications")
-
-local curl_buf_name = "Curl Command"
-local output_buf_name = "Curl Output"
+local cache = require("curl.cache")
+local output_buffer = require("curl.output")
 
 M.open_curl_tab = function()
-	local curl_buffer = buffers.get_or_create_buffer(curl_buf_name)
-	vim.api.nvim_set_option_value("filetype", "sh", { buf = curl_buffer })
-
-	local output_buffer = buffers.get_or_create_buffer(output_buf_name)
-	vim.api.nvim_set_option_value("filetype", "json", { buf = output_buffer })
-
-	vim.cmd("tabnew")
-	vim.api.nvim_win_set_buf(0, curl_buffer)
-
-	local cached_commands = cache.load_cached_commands()
-	if #cached_commands > 0 then
-		vim.api.nvim_buf_set_lines(curl_buffer, 0, -1, false, cached_commands)
-	end
-
-	vim.cmd("vsplit")
-	vim.cmd("wincmd l")
-
-	vim.api.nvim_win_set_buf(0, output_buffer)
-
-	vim.cmd("wincmd h")
+	local curl_buffer = buffers.open_curl_tab()
 
 	vim.api.nvim_buf_set_keymap(
 		curl_buffer,
@@ -39,43 +17,18 @@ M.open_curl_tab = function()
 	)
 end
 
-local get_curl_command = function()
-	local left_buf = vim.api.nvim_get_current_buf()
-	local lines = vim.api.nvim_buf_get_lines(left_buf, 0, -1, false)
-
+M.execute_curl = function()
+	local cursor_pos, lines = buffers.get_command_buffer_and_pos()
+	-- todo: cache on insert instead
 	cache.save_commands_to_cache(lines)
 
-	local cursor_pos = vim.api.nvim_win_get_cursor(0)[1]
-	local result = parser.parse_curl_command(cursor_pos, lines)
-	result = result .. " -s -S"
-
-	return result
-end
-
-M.execute_curl = function()
-	local curl_command = get_curl_command()
+	local curl_command = parser.parse_curl_command(cursor_pos, lines)
 
 	local output = ""
 	local error = ""
-
 	local _ = vim.fn.jobstart(curl_command, {
 		on_exit = function(_, _, _)
-			local right_buf = buffers.get_or_create_buffer(output_buf_name)
-			if not right_buf then
-				notify.error("Could not find the output buffer, try relaunching the curl tab")
-				return
-			end
-
-			if error ~= "" then
-				notify.error("Curl failed")
-				vim.api.nvim_buf_set_lines(right_buf, 0, -1, false, { error })
-				return
-			end
-
-			vim.api.nvim_buf_set_lines(right_buf, 0, -1, false, { output })
-			vim.api.nvim_buf_call(right_buf, function()
-				vim.cmd("%!jq '.'")
-			end)
+			output_buffer.write_output(output, error)
 		end,
 		on_stdout = function(_, data, _)
 			output = output .. vim.fn.join(data)
--- lua/curl/buffers.lua
+++ lua/curl/buffers.lua
@@ -1,6 +1,10 @@
 M = {}
+local cache = require("curl.cache")
 
-M.get_or_create_buffer = function(name)
+CURL_COMMAND_BUF_NAME = "Curl Command"
+CURL_OUTPUT_BUF_NAME = "Curl Output"
+
+local function get_or_create_buffer(name)
 	local buf = vim.fn.bufnr(name .. "$")
 
 	if buf == -1 then
@@ -9,6 +13,43 @@ M.get_or_create_buffer = function(name)
 	end
 
 	return buf
+end
+
+M.get_or_create_buffer = function(name)
+	get_or_create_buffer(name)
+end
+
+M.open_curl_tab = function()
+	local curl_buffer = get_or_create_buffer(CURL_COMMAND_BUF_NAME)
+	vim.api.nvim_set_option_value("filetype", "sh", { buf = curl_buffer })
+
+	local output_buffer = get_or_create_buffer(CURL_OUTPUT_BUF_NAME)
+	vim.api.nvim_set_option_value("filetype", "json", { buf = output_buffer })
+
+	vim.cmd("tabnew")
+	vim.api.nvim_win_set_buf(0, curl_buffer)
+
+	local cached_commands = cache.load_cached_commands()
+	if #cached_commands > 0 then
+		vim.api.nvim_buf_set_lines(curl_buffer, 0, -1, false, cached_commands)
+	end
+
+	vim.cmd("vsplit")
+	vim.cmd("wincmd l")
+
+	vim.api.nvim_win_set_buf(0, output_buffer)
+
+	vim.cmd("wincmd h")
+	return curl_buffer
+end
+
+M.get_command_buffer_and_pos = function()
+	local left_buf = vim.api.nvim_get_current_buf()
+	local lines = vim.api.nvim_buf_get_lines(left_buf, 0, -1, false)
+
+	local cursor_pos = vim.api.nvim_win_get_cursor(0)[1]
+
+	return cursor_pos, lines
 end
 
 return M
