3d639d284c706947070610933d70be1bbd015c70
Øystein Sandvik <oysandvik94@gmail.com>
2024-07-26 16:15:14 +0200

    ci: fix tests in github actions

--- tests/busted.lua
+++ tests/busted.lua
@@ -6,5 +6,10 @@ load(vim.fn.system("curl -s https://raw.githubusercontent.com/folke/lazy.nvim/ma
 vim.cmd("filetype plugin on")
 -- Setup lazy.nvim
 require("lazy.minit").busted({
-	spec = { dir = vim.uv.cwd(), config = true, dependencies = { "nvim-lua/plenary.nvim" } },
+	spec = {
+		dir = vim.uv.cwd(),
+		config = true,
+		dependencies = { "nvim-lua/plenary.nvim" },
+		"nvim-treesitter/nvim-treesitter",
+	},
 })
