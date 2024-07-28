68f9a23806ebef5b3f0ecd5a969e44840e52b2b4
Øystein Sandvik <oysandvik94@gmail.com>
2024-07-28 12:39:33 +0200

    fix: dont crash if bash treesitter parser is not installed

--- ftplugin/curl.lua
+++ ftplugin/curl.lua
@@ -1,56 +1,6 @@
 vim.bo.syntax = "sh"
 vim.bo.commentstring = "# %s"
 
-vim.treesitter.language.register("bash", "curl")
-
--- Custom highlights for curl-specific elements
-local query = vim.treesitter.query.parse(
-	"bash",
-	[[
-      ;; Highlight the 'curl' command
-      ((command_name) @curl (#eq? @curl "curl"))
-
-      ;; Highlight curl options
-      ((word) @curl_option (#match? @curl_option "^-"))
-
-      ;; Highlight HTTP methods
-      ((word) @http_method (#match? @http_method "^(GET|POST|PUT|DELETE|PATCH|HEAD|OPTIONS|TRACE|CONNECT)$"))
-
-      ;; Highlight URLs
-      ((word) @url (#match? @url "^https?://"))
-    ]]
-)
-
-local bufnr = 0
-local parser = vim.treesitter.get_parser(bufnr, "bash")
-local tree = parser:parse()[1]
-local root = tree:root()
-
-for id, node in query:iter_captures(root, bufnr, 0, -1) do
-	local name = query.captures[id]
-	local start_row, start_col, end_row, end_col = node:range()
-
-	if name == "curl" then
-		vim.api.nvim_buf_add_highlight(bufnr, -1, "CurlKeyword", start_row, start_col, end_col)
-	elseif name == "curl_option" or name == "http_method" then
-		vim.api.nvim_buf_add_highlight(bufnr, -1, "CurlFunction", start_row, start_col, end_col)
-	elseif name == "url" then
-		vim.api.nvim_buf_add_highlight(bufnr, -1, "CurlUrl", start_row, start_col, end_col)
-	end
-end
-
-vim.api.nvim_set_hl(0, "CurlUrl", {
-	link = "@text",
-})
-
-vim.api.nvim_set_hl(0, "CurlFunction", {
-	link = "@function",
-})
-
-vim.api.nvim_set_hl(0, "CurlKeyword", {
-	link = "@operator",
-})
-
 local config = require("curl.config")
 local execute_mapping = config.get("mappings")["execute_curl"]
 vim.api.nvim_buf_set_keymap(
@@ -66,3 +16,10 @@ vim.api.nvim_create_autocmd({ "InsertLeave", "TextChanged" }, {
 	buffer = 0,
 	nested = true,
 })
+
+vim.treesitter.language.register("bash", "curl")
+local bash_lang_installed, _ = pcall(vim.treesitter.get_parser, 0, "bash")
+if bash_lang_installed then
+	require("curl.treesitter").add_treesitter_highlighting()
+	return
+end
---
+++ lua/curl/treesitter.lua
@@ -0,0 +1,53 @@
+local M = {}
+
+M.add_treesitter_highlighting = function()
+	-- Custom highlights for curl-specific elements
+	local query = vim.treesitter.query.parse(
+		"bash",
+		[[
+      ;; Highlight the 'curl' command
+      ((command_name) @curl (#eq? @curl "curl"))
+
+      ;; Highlight curl options
+      ((word) @curl_option (#match? @curl_option "^-"))
+
+      ;; Highlight HTTP methods
+      ((word) @http_method (#match? @http_method "^(GET|POST|PUT|DELETE|PATCH|HEAD|OPTIONS|TRACE|CONNECT)$"))
+
+      ;; Highlight URLs
+      ((word) @url (#match? @url "^https?://"))
+    ]]
+	)
+
+	local bufnr = 0
+	local parser = vim.treesitter.get_parser(bufnr, "bash")
+	local tree = parser:parse()[1]
+	local root = tree:root()
+
+	for id, node in query:iter_captures(root, bufnr, 0, -1) do
+		local name = query.captures[id]
+		local start_row, start_col, end_row, end_col = node:range()
+
+		if name == "curl" then
+			vim.api.nvim_buf_add_highlight(bufnr, -1, "CurlKeyword", start_row, start_col, end_col)
+		elseif name == "curl_option" or name == "http_method" then
+			vim.api.nvim_buf_add_highlight(bufnr, -1, "CurlFunction", start_row, start_col, end_col)
+		elseif name == "url" then
+			vim.api.nvim_buf_add_highlight(bufnr, -1, "CurlUrl", start_row, start_col, end_col)
+		end
+	end
+
+	vim.api.nvim_set_hl(0, "CurlUrl", {
+		link = "@text",
+	})
+
+	vim.api.nvim_set_hl(0, "CurlFunction", {
+		link = "@function",
+	})
+
+	vim.api.nvim_set_hl(0, "CurlKeyword", {
+		link = "@operator",
+	})
+end
+
+return M
